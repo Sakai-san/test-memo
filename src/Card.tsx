6d2ecfd0c12280a28fd604dd0d5263a1a7f22f94
Sakai Rubattel Toma <o_edo@hotmail.com>
2020-12-04 23:53:45 +0100

    Typing.

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,16 +1,21 @@
 import React, { FunctionComponent, memo, ComponentType } from "react";
 import { connect } from "react-redux";
+import { createFactory } from "react";
 import { AppState } from "./store/types";
+import { compose } from "redux";
 
-const withNoRerendering = <P extends {}>(WrappedComponent: ComponentType<P>) =>
+const withNoRerendering = <P extends {} & { doPreventRerendering?: boolean }>(
+  WrappedComponent: ComponentType<P>
+) =>
   memo(
     (props) => <WrappedComponent {...(props as P)} />,
-    (prevProps: P, nextProps: P) => true
+    (prevProps: P, nextProps: P) =>
+      nextProps?.doPreventRerendering === true ? true : false
   );
 
-interface Props {
+type Props = {
   firstName?: string;
-}
+} & { doPreventRerendering?: boolean };
 
 const Card: FunctionComponent<Props> = ({ firstName }) => {
   return <div style={{ border: "2px solid red" }}>Hello {firstName}</div>;
@@ -18,4 +23,15 @@ const Card: FunctionComponent<Props> = ({ firstName }) => {
 
 export default connect((state: AppState) => ({
   firstName: state?.firstName,
+  doPreventRerendering: true,
 }))(withNoRerendering(Card));
+
+/*
+export default compose(
+  connect((state: AppState) => ({
+    firstName: state?.firstName,
+  })),
+  (BaseComponent) => (props) =>
+    createFactory(BaseComponent)({ ...props, do: true })
+)(withNoRerendering(Card));
+*/
