9d04eab19db29d395e982292f909a736bf31832c
Sakai Rubattel Toma <o_edo@hotmail.com>
2020-12-04 23:42:01 +0100

    Simplify HOC.

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,8 +1,5 @@
 import React, { FunctionComponent, memo, ComponentType } from "react";
-import { createFactory } from "react";
 import { connect } from "react-redux";
-import { compose } from "redux";
-import { isPropertySignature } from "typescript";
 import { AppState } from "./store/types";
 
 const withNoRerendering = <P extends {}>(WrappedComponent: ComponentType<P>) =>
@@ -19,15 +16,6 @@ const Card: FunctionComponent<Props> = ({ firstName }) => {
   return <div style={{ border: "2px solid red" }}>Hello {firstName}</div>;
 };
 
-export default compose(
-  connect((state: AppState) => ({
-    firstName: state?.firstName,
-  }))
-)(withNoRerendering(Card));
-
-/*
 export default connect((state: AppState) => ({
   firstName: state?.firstName,
-  doPreventRerendering: true,
-}))(withcontrolRerendering(Card));
-*/
+}))(withNoRerendering(Card));
