10d14c4d1ff7b676009ce39b198621140dca12cf
Sakai Rubattel Toma <o_edo@hotmail.com>
2020-12-07 07:22:32 +0100

    Works by combining both useselector and memo.

--- src/Card1.tsx
+++ src/Card1.tsx
@@ -1,17 +1,19 @@
 import React, { FunctionComponent } from "react";
 import { useSelector } from "react-redux";
+import withNoRerendering from "./HOC/withNoRerendering";
 
 import { AppState } from "./store/types";
 
 type Props = {};
 
 const Card1: FunctionComponent<Props> = () => {
+  console.log("Card bleue");
   const firstName = useSelector(
     (state: any) => state.firstName,
-    (prevProps: Props, nextProps: Props) => false
+    (prevProps: Props, nextProps: Props) => true
   );
 
   return <div style={{ border: "2px solid blue" }}>Hello {firstName}</div>;
 };
 
-export default Card1;
+export default withNoRerendering(Card1);
